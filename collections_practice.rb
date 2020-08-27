fa24c39ee148f3f6b71319097754a274c4d75a79
Haidy  Farahat <haidyfarahat@gmail.com>
2020-08-27 15:25:58 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,15 @@
-
+def sort_array_asc (array)
+  array.sort
+end
+def sort_array_desc (array)
+  array.sort.reverse
+end
+def sort_array_char_count (array)
+  array.sort{|a,b| a.length <=> b.length}
+end
+def swap_elements (array)
+  array [0], array [1], array [2] = array [0], array [2], array [1]
+end
+def reverse_array (array)
+  array.reverse
+end
